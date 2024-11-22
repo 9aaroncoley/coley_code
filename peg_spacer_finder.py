7e1bad57744edc1f823e8c16accba157cadf9958
9aaroncoley <144277179+9aaroncoley@users.noreply.github.com>
2024-11-22 19:51:22 +0000

    lot of changes but haven't really changed the function too much, about to actually implement big changes

--- peg_spacer_finder.py
+++ peg_spacer_finder.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 
 # Using a raw string to ensure backslashes are interpreted correctly
-data = pd.read_csv(r"C:\Users\that9\OneDrive\Documents\PrimeDesign_Pooled_20241031_07.46.21.51.csv")
+data = pd.read_csv(r"PrimeDesign_Pooled_20241031_07.46.21.51.csv")
 
 # data has all that data 
 # for now let's just try to find peg extensions in the original sequence
@@ -17,19 +17,51 @@ flips = data["Strand"]
 adjusted_sequences = []
 peg_extensions = data["pegRNA_extension"]
 locations = [] # add tuples of locations
+spacer_sequences = data["Spacer_sequence"]
+pams = data["PAM_sequence"]
+flipped_sequences = [] # tuple of flipped reference and alternate sequences
 # need to flip some of the strands
 
 # NEED TO make cleaned_sequences contain reference_sequence, alternate_sequence
+# let's just make a reference and alternate for every strand
+
+#reference is keep left of / and delete the right
+
+
+reference_strands = []
+for sequence in original_sequences: # THIS WORK
+    temp = re.sub(r'/(.*?)\)', '', sequence) # hopefully removing just the right
+    temp = temp.replace( '(', '') # removing )
+    reference_strands.append(temp)
+
+alternate_strands = []
+for sequence in original_sequences: # THIS WORKS
+    temp = re.sub(r'\((.*?)/', '', sequence) # hopefully removing just the left
+    temp = temp.replace( ')', '') # removing )
+    alternate_strands.append(temp)
+
+# now we have all the reference and alternate strands, now we need the spam
+spacer_pams = [f"{s}{p}" for s, p in zip(spacer_sequences, pams)]  # Combined spacer and PAM WORKS
+
+# we have reference sequences, alternate sequence, spams, and peg extensions
+
+flipper = str.maketrans("ACTG", "TGAC","()")
+for reference, alternate in zip(reference_strands, alternate_strands): # now we have the flipped strands
+    t1 = reference.translate(flipper)[::-1]
+    t2 = alternate.translate(flipper)[::-1]
+    flipped_sequences.append((t1, t2))
+
+
+
+
+
 pattern = r'/.*?\)'
 cleaned_sequences = [re.sub(pattern, '', seq) for seq in original_sequences]
-spacer_sequences = data["Spacer_sequence"]
-pams = data["PAM_sequence"]
-# spacer_pams = []
-spacer_pams = [f"{s}{p}" for s, p in zip(spacer_sequences, pams)]  # Combined spacer and PAM
+
 '''for s,p in zip(spacer_sequences,pams):
     mixed = spacer_sequences + pams
     spacer_pams.append(mixed) # spacer_pams has spacers + pams'''
-flipper = str.maketrans("ACTG", "TGAC","()")
+
 for sequence, sign in zip(cleaned_sequences, flips): # pairs each sequence with its sign
     if (sign == "-"): # need to make the strand antiparallel and complementary 
         adjusted_sequences.append(sequence.translate(flipper)[::-1])
@@ -68,7 +100,7 @@ for flip, clean, sequence, peg, spacer in zip(flips, cleaned_sequences, adjusted
 locations_df = pd.DataFrame(locations, columns = ['Peg_Start', 'Peg_End', 'SPAM_Start', 'SPAM_End'])
 output_file_path = r"C:\Users\that9\OneDrive\Documents\output_locations.csv"
 locations_df.to_csv(output_file_path, index=False)
-print(locations)
+#print(locations)
 
 
 
